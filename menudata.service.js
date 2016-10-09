98dd131326f435833ca2495310b66fbbcbc8ba64
nikita <nikita1792@yahoo.com>
2016-10-09 22:09:19 +0530

    Second Revision

--- menudata.service.js
+++ menudata.service.js
@@ -9,6 +9,7 @@ function MenuDataService($http, ApiBasePath) {
 	var service = this;
 	var categories=[];
   var menuItems=[];
+	var returnedMenuItems=[];
 	service.getAllCategories = function () {
 		return $http({method: "GET",url: (ApiBasePath + "/categories.json")}).then(function (response) {
       categories=response.data;
@@ -16,14 +17,8 @@ function MenuDataService($http, ApiBasePath) {
 		});
 	};
   service.getItemsForCategory=function(categoryShortName){
-    return $http({method: "GET",url: (ApiBasePath + "/menu_items.json")}).then(function (response) {
-			var returnedMenuItems=[];
-      menuItems=response.data.menu_items;
-			for(var i=0;i<menuItems.length;i++){
-				if(menuItems[i].short_name.indexOf(categoryShortName)!=-1){
-returnedMenuItems.push(menuItems[i]);
-				}
-			}
+    return $http({method: "GET",url: (ApiBasePath + "/menu_items.json/category="+categoryShortName)}).then(function (response) {
+      returnedMenuItems=response.data.menu_items;
       return returnedMenuItems;
     });
   };
